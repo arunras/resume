e96ccbe6074b83f8bfecbcffb15dae71e98d8655
Arun <prith000@citymail.cuny.edu>
2017-05-05 00:04:18 -0400

    update experience

--- src/experience.js
+++ src/experience.js
@@ -25,24 +25,24 @@ class Experience extends Component{
                                 <CardText className="timeline">Oxford, MA</CardText>
                             </Col>
                         </Row>
-                        {/*
+                        {
                         <CardText>
                             <ul>
-                                <li>Assembled, installed and inspected components, diodes, units, and fiber optics.</li>
-                                <li>Positioned, aligned, repaired and adjusted pieces to ensure conformance to specifications.</li>
-                                <li>Recorded serial numbers and voltages for products and modified products.</li>
-                                <li>Used electronic, hand, power, and pneumatic tools.</li>
-                                <li>Tested a full range of technical assembly, and troubleshooted.</li>
-                                <li>Soldering.</li>
+                                <li>Supported of software development for new product and existing products.</li>
+                                <li>Implemented and added features to existing products.</li>
+                                <li>Updated and maintained existing modules.</li>
+                                <li>Provided support in system testing and bug fixing activities.</li>
+                                <li>Designed, coded, and debugged systems in C/C++.</li>
+                                <li>Analyzed test data and reports to determine if design meets functional and performance specifications.</li>
                             </ul>
                         </CardText>
-                        */}
+                        }
                     </CardBlock>
                 </Card>
                 
                 <Card className="info_content border-0">
                     <CardBlock> 
-                        <CardText className="resume-major">Web Developer</CardText>
+                        <CardText className="resume-major">Computer Lab Assistant</CardText>
                         <CardText className="org-name">Mahidol University</CardText>
                         <Row>
                             <Col lg="3">
@@ -52,17 +52,15 @@ class Experience extends Component{
                                 <CardText className="timeline">Bangkok, Thailand</CardText>
                             </Col>
                         </Row>
-                        {/*
+                        {
                         <CardText>
                             <ul>
-                                <li>Provided technical support such as hardware and software for students and professors 
-        related to computers, software and other peripherals.</li>
-                                <li>Maintained and performed preventative maintenance on lab equipments including printers 
-        and computers for regular use.</li>
-                                <li>Responded to students’ needs regarding quota limitation and payment. </li>
+                                <li>Provided technical support such as hardware and software for students and professors.</li>
+                                <li>Maintained laboratory software, test materials and hardware to ensure security.</li>
+                                <li>Responded to queries regarding quota limitation, payment, and printing support.</li>
                             </ul>
                         </CardText>
-                        */}
+                        }
                     </CardBlock>
                 </Card>
 
@@ -78,16 +76,21 @@ class Experience extends Component{
                                 <CardText className="timeline">Phnom Penh, Cambodia</CardText>
                             </Col>
                         </Row>
-                        {/*
+                        {
                         <CardText>
                             <ul>
-                                <li>Designed, prototyped and developed websites using language scripts.</li>
-                                <li>Improved website’s layouts.</li>
-                                <li>Used PHP, MySQL, HTML, CSS, JavaScript, JQuery, CodeIgniter, WordPress, Social Network’s APIs, and Google Maps API.</li>
+                                <li>Assisted in designing, architecture and development of web applications.</li>
+                                <li>Implemented procedures for creation of wireframes and prototypes for new user interfaces.</li>
+                                <li>Supported technical team members in development of functional and visual user interfaces.</li>
+                                <li>Assisted in designing, configured, and development of relational databases for back-end.</li>
+                                <li>Participated in testing and debugging of new capabilities and systems.</li>
+                                <li>Provided technical assistance for development and deployment of sites.</li>
+                                <li>Participated in continuous learning of web development methods and UI designing trends.</li>
                                 <li>Worked with development teams in Cambodia and Japan.</li>
+                                <li>Software Stack: PHP, MySQL, HTML, CSS, JavaScript, JQuery, CodeIgniter, WordPress, Social Network’s API, and Google Maps API.</li>
                             </ul>
                         </CardText>
-                        */}
+                        }
                     </CardBlock>
                 </Card>
             </Card>
