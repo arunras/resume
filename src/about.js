0dcd8ea0e4fd95cc98888ec862339225710ed63a
Arun <prith000@citymail.cuny.edu>
2017-05-08 17:06:54 -0400

    update about

--- src/about.js
+++ src/about.js
@@ -8,7 +8,7 @@ class About extends Component{
        return(
             <Card className="info_content border-0">
                 <CardBlock>
-                    <CardText className="resume-about">I am a self-driven full stack developer. I have been digging in deep into solid computer science theory, and broad programming knowledge base. I am always inside the terminal, and can say that I am more on the Dark Side of the Force. I work mostly with C/C++ for the algorithms to solve NP complexity and JavaScript to create beautiful functional web applications.
+                    <CardText className="resume-about">I am a self-driven full stack developer. I have been digging in deep into solid computer science theory, and broad programming knowledge base. I am always inside the terminal. I work mostly with C/C++ for the algorithms to optimize NP complexity and JavaScript to create beautiful functional web applications.
                     </CardText>
                 </CardBlock>
             </Card>
